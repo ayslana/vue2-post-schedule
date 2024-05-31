05f3c942f13b6d1d2beaecea2703378601233dc9
ayslana <ayslanariene@gmail.com>
2024-05-31 04:17:02 -0300

    feat: add ScheduleListView component for displaying a list of schedules

---
+++ src/views/ScheduleListView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p class="left-align">Listagem de agendamento</p>
+    <TableList />
+  </div>
+</template>
+
+<script>
+import TableList from "@/components/global/TableList.vue";
+
+export default {
+  name: "ScheduleListView",
+  components: {
+    TableList,
+  },
+};
+</script>
+
+<style>
+.left-align {
+  text-align: left;
+  font-weight: bold;
+  margin-bottom: 50px;
+}
+</style>
