538d971a2f49464a3c1e96bd36b208e88f001461
ayslana <ayslanariene@gmail.com>
2024-05-31 04:12:34 -0300

    refactor: update App.vue structure and styling

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <NavBar />
+    <v-main>
+      <router-view
+        style="
+          min-height: calc(100vh - 120px);
+          padding: 120px 36px 0px 36px;
+          background-color: #f8f8f8;
+        "
+      />
+    </v-main>
   </div>
 </template>
 
+<script>
+import NavBar from "@/components/global/NavBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Poppins, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  background-color: #f8f8f8;
 }
 </style>
