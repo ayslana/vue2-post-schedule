fb5d18714791403c320679add7a151abfbf4fca0
ayslana <ayslanariene@gmail.com>
2024-05-31 04:16:18 -0300

    feat: add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "Home",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/HomeView.vue"),
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/agendamento",
+    name: "Agendamento",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "../views/ScheduleView.vue"),
+  },
+  {
+    path: "/listagem-agendamento",
+    name: "Listagem de Agendamento",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/ScheduleListView.vue"),
+  },
+  {
+    path: "*",
+    redirect: { name: "Home" },
   },
 ];
 
