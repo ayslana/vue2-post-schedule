2184bc479921a832b03677ebad341be4bd25e1f7
ayslana <ayslanariene@gmail.com>
2024-05-31 04:13:41 -0300

    feat: add PublicationDateContainer component for publication date functionality

---
+++ src/components/PublicationDateContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <BaseContainer title="Data de publicação">
+      <DateTimeInput />
+    </BaseContainer>
+  </div>
+</template>
+<script>
+import BaseContainer from "@/components/global/BaseContainer.vue";
+import DateTimeInput from "@/components/global/DateTimeInput.vue";
+
+export default {
+  name: "PublicationDateContainer",
+  components: {
+    BaseContainer,
+    DateTimeInput,
+  },
+};
+</script>
+<style lang=""></style>
